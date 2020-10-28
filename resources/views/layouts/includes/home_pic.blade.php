3b3367e76031bb3c665ec0dbecb9786302dea852
khaled-sayed <62744648+khaled-sayed@users.noreply.github.com>
2020-10-28 09:25:15 +0400

    Finishing Home Page

--- resources/views/layouts/includes/home_pic.blade.php
+++ resources/views/layouts/includes/home_pic.blade.php
@@ -2,10 +2,13 @@
     <div class="overlay">
         <div class="container-fluid">
             <div class="background-text">
-                <p>Start Learning {{\App\Course::count()}} Courses For <span class="text-blue">Free</span></p>
+                <p>Start Learning <span class="text-blue">{{\App\Course::count()}}</span> Courses For <span class="text-blue">Free</span></p>
                 <p>More Than {{\App\User::count()}} users have In enrolled in {{\App\Course::count()}} Courses in {{\App\Track::count()}} Tracks</p>
-                <a class="btn btn-primary" href="/register">Start Learning</a>
+                <div class="buttons">
+                    <a class="" href="/register">Start Learning</a>
+                    <a class="custom-btn" href="#">My Courses</a>
+                </div>
             </div>
         </div>
-    </div> 
+    </div>
 </div>
